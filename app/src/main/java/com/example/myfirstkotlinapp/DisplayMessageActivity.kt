c0fd86f92bc9282a06092f3c3e5ccb0abda0b1cf
Kyle Lemons <klemons15@hotmail.com>
2019-10-30 08:28:59 -0500

    created a random answer function to act as an "eight ball"

--- app/src/main/java/com/example/myfirstkotlinapp/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/DisplayMessageActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.widget.TextView
+import java.util.concurrent.ThreadLocalRandom
+import kotlin.random.Random
 
 class DisplayMessageActivity : AppCompatActivity() {
 
@@ -18,5 +20,30 @@ class DisplayMessageActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.textView).apply {
             text = message
         }
+
+        //Randomize the "8 Ball message"
+        var answer1 = "Yes, of course!"
+        var answer2 = "Surely not!"
+        var answer3 = "Maybe, who knows?"
+        var random = (1..3).shuffled().last()
+        if (random == 1)
+        {
+            val answerView = findViewById<TextView>(R.id.textView2).apply {
+                text = answer1
+            }
+        }
+        else if (random == 2)
+        {
+            val answerView = findViewById<TextView>(R.id.textView2).apply {
+                text = answer2
+            }
+        }
+        else if (random == 3) {
+            val answerView = findViewById<TextView>(R.id.textView2).apply {
+                text = answer3
+            }
+        }
+
     }
+
 }
