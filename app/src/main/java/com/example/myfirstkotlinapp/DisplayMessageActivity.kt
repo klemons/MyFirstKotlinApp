378e4f77f3c49b9ce98273ac746a087ac5f25225
Kyle Lemons <klemons15@hotmail.com>
2019-11-15 13:53:41 -0600

    the eight different words input are now placed into the displayed story correcty. All left now is cleaning up constraints and fancy UI

--- app/src/main/java/com/example/myfirstkotlinapp/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/DisplayMessageActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.widget.TextView
+import kotlinx.android.synthetic.*
 import java.util.concurrent.ThreadLocalRandom
 import kotlin.random.Random
 
@@ -13,36 +14,31 @@ class DisplayMessageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
 
-        val textView = findViewById<TextView>(R.id.textView)
+        val textView1 = findViewById<TextView>(R.id.input1)
+        val textView2 = findViewById<TextView>(R.id.input2)
+        val textView3 = findViewById<TextView>(R.id.input3)
+        val textView4 = findViewById<TextView>(R.id.input4)
+        val textView5 = findViewById<TextView>(R.id.input5)
+        val textView6 = findViewById<TextView>(R.id.input6)
+        val textView7 = findViewById<TextView>(R.id.input7)
+        val textView8 = findViewById<TextView>(R.id.input8)
 
         var wordHolder = arrayListOf<String>()
         wordHolder = intent.getStringArrayListExtra("wordList")
 
-        for (item in wordHolder)
-            textView.append(item)
+        for (i in wordHolder.indices) {
+            when (i) {
+                0 -> textView1.append(wordHolder[0])
+                1 -> textView2.append(wordHolder[1])
+                2 -> textView3.append(wordHolder[2])
+                3 -> textView4.append(wordHolder[3])
+                4 -> textView5.append(wordHolder[4])
+                5 -> textView6.append(wordHolder[5])
+                6 -> textView7.append(wordHolder[6])
+                7 -> textView8.append(wordHolder[7])
+            }
+        }
 
-        //Randomize the "8 Ball message"
-        var answer1 = "Yes, of course!"
-        var answer2 = "Surely not!"
-        var answer3 = "Maybe, who knows?"
-        var random = (1..3).shuffled().last()
-        if (random == 1)
-        {
-            val answerView = findViewById<TextView>(R.id.textView2).apply {
-                text = answer1
-            }
-        }
-        else if (random == 2)
-        {
-            val answerView = findViewById<TextView>(R.id.textView2).apply {
-                text = answer2
-            }
-        }
-        else if (random == 3) {
-            val answerView = findViewById<TextView>(R.id.textView2).apply {
-                text = answer3
-            }
-        }
 
     }
 
